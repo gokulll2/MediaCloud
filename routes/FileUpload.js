e446537df5a2c63fbf6f7d9d92d552f1f78bd07e
Gokul <gokulsethi69@gmail.com>
2023-07-24 02:07:40 +0530

    main

--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {localFile,imageUpload,videoUpload} = require("../controllers/fileupload");
+const {localFile,imageUpload,videoUpload,imageSizeReducer} = require("../controllers/fileupload");
 
 router.post('/imageUpload',imageUpload)
 router.post('/videoUpload' , videoUpload);
-// router.post('/imageReducer' , imageReducer);
+router.post('/imageSizeReducer' , imageSizeReducer);
 router.post('/localFileupload' , localFile);
 
 module.exports = router;
